aead64ebfa37725c362a638665ab9ea76b08d33c
Colten11 <101116326+Colten11@users.noreply.github.com>
2022-03-29 09:00:27 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,14 @@
 def main():
     #your code goes here
-    print("0")
+    number = float(input("Enter a number: "))
+    if number%3 == 0 and number%5==0:
+        print("fizzbuzz")
+    elif number%3 == 0:
+        print("fizz")
+    elif number%5 == 0:
+        print("buzz")
+    else:
+        print(number)
 
 if __name__ =='__main__':
     main()
